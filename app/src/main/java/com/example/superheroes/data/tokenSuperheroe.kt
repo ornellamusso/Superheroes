8c1bcb9da5e73db20f4b170bef8d3af016b70598
ornella <ornellamussog@gmail.com>
2024-11-12 19:00:08 +0100

    copying the library - insert token API . create 3 pgs.kt

---
+++ app/src/main/java/com/example/superheroes/data/tokenSuperheroe.kt
@@ -0,0 +1,2 @@
+package com.example.superheroes.data
+val tokenSuperheroe="760d19b85103ac0ac53c3470ec0c0a53"
---
+++ app/src/main/java/com/example/superheroes/utils/RetrofitProvider.kt
@@ -0,0 +1,33 @@
+package com.example.superheroes.utils
+
+import com.example.superheroes.data.tokenSuperheroe
+import retrofit2.Converter
+import retrofit2.Retrofit
+
+class SuperheroeService {
+
+}
+
+class GsonCoverterFactory {
+    companion object {
+        fun create() {
+        }
+    }
+
+}
+
+class RetrofitProvider {
+    companion object {
+        fun getRetrofit(): SuperheroeService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://superheroapi.com/api/${tokenSuperheroe}/")
+                .addConverterFactory(GsonCoverterFactory.create())
+                .build()
+
+            val SuperheroService = Unit
+            return retrofit.create(SuperheroService::class.java.())
+        }
+
+    }
+}
+
